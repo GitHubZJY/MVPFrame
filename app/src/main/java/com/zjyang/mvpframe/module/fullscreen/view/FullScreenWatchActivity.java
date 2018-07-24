1bfa9b3e2530cc6a05d354ccd41d82e82c0a902d
zhengjiayang <742155745@qq.com>
2018-07-24 14:12:39 +0800

    补提

--- app/src/main/java/com/zjyang/mvpframe/module/fullscreen/view/FullScreenWatchActivity.java
+++ app/src/main/java/com/zjyang/mvpframe/module/fullscreen/view/FullScreenWatchActivity.java
@@ -2,30 +2,21 @@ package com.zjyang.mvpframe.module.fullscreen.view;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Window;
 import android.widget.TableLayout;
 
-import com.example.zjy.player.ui.PlayerListener;
 import com.example.zjy.player.ui.VideoFrame;
-import com.example.zjy.player.ui.YPlayerView;
 import com.zjyang.mvpframe.R;
-import com.zjyang.mvpframe.event.FullScreenExitEvent;
 import com.zjyang.mvpframe.module.base.BaseActivity;
 import com.zjyang.mvpframe.module.base.BasePresenter;
-import com.zjyang.mvpframe.module.home.discover.model.VideoFramesModel;
 import com.zjyang.mvpframe.module.home.model.bean.VideoInfo;
-import com.zjyang.mvpframe.utils.HandlerUtils;
 import com.zjyang.mvpframe.utils.LogUtil;
-
-import org.greenrobot.eventbus.EventBus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import tv.danmaku.ijk.media.player.IMediaPlayer;
 
 /**
  * Created by 74215 on 2018/4/15.
@@ -66,7 +57,6 @@ public class FullScreenWatchActivity extends BaseActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         mVideoInfo = bundle.getParcelable(VIDEO_INFO);
-        //mVideoInfo = VideoFramesModel.getInstance().getCurPlayVideo();
         mPlayView.setHudView(mHudView);
         mPlayView.setVideoUrl(mVideoInfo.getVideoUrl());
         LogUtil.d(TAG, "URL: " + mVideoInfo.getVideoUrl());
