6bb37c70b5d0ba8d4251c4e2752c94fd381f6449
zhengjiayang <742155745@qq.com>
2018-07-30 20:49:25 +0800

    导入高德定位sdk

---
+++ app/src/main/java/com/zjyang/mvpframe/utils/LocationUtils.java
@@ -0,0 +1,71 @@
+package com.zjyang.mvpframe.utils;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationListener;
+import com.zjyang.mvpframe.application.AppApplication;
+
+/**
+ * Created by zhengjiayang on 2018/7/30.
+ */
+
+public class LocationUtils {
+
+    public static final String TAG = "LocationUtils";
+
+    AMapLocationClient mLocationClient;
+
+    private static LocationUtils mInstance = null;
+    private LocationUtils(){
+        //初始化定位
+        mLocationClient = new AMapLocationClient(AppApplication.getContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+    }
+    public static LocationUtils getInstance(){
+        if(mInstance == null){
+            mInstance = new LocationUtils();
+        }
+        return mInstance;
+    }
+
+    /**
+     * 启动定位
+     */
+    public void startLocation(){
+        mLocationClient.startLocation();
+    }
+
+    /**
+     * 停止定位
+     */
+    public void stopLocation(){
+        mLocationClient.stopLocation();
+    }
+
+    /**
+     * 定位回调监听器
+     */
+    public static AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+            // 从这里开始就会持续回调
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+                    amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    double currentLat = amapLocation.getLatitude();//获取纬度
+                    double currentLon = amapLocation.getLongitude();//获取经度
+                    String address = amapLocation.getAddress();
+                    amapLocation.getAccuracy();//获取精度信息
+                    LogUtil.i(TAG, "currentLat : " + currentLat + " currentLon : " + currentLon + " address: " + address);
+                } else {
+                    //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                    LogUtil.e(TAG, "location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+                }
+            }
+        }
+    };
+}
