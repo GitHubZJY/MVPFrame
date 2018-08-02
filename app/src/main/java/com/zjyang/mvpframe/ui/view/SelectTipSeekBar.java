64daf15db7ee7d9af4ba1a846049b059fc65ccc7
zhengjiayang <742155745@qq.com>
2018-08-02 18:03:57 +0800

    优化滑动选择预览图的Seekbar

--- app/src/main/java/com/zjyang/mvpframe/ui/view/SelectTipSeekBar.java
+++ app/src/main/java/com/zjyang/mvpframe/ui/view/SelectTipSeekBar.java
@@ -2,11 +2,14 @@ package com.zjyang.mvpframe.ui.view;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 import android.widget.SeekBar;
 
 import com.zjyang.mvpframe.R;
@@ -16,6 +19,7 @@ import java.lang.reflect.Field;
 
 /**
  * Created by zhengjiayang on 2018/8/1.
+ * 带提示的seekbar
  */
 
 public class SelectTipSeekBar extends FrameLayout implements View.OnTouchListener, SeekBar.OnSeekBarChangeListener {
@@ -24,13 +28,16 @@ public class SelectTipSeekBar extends FrameLayout implements View.OnTouchListene
 
     private SeekBar mSeekBar;
     private SeekBarTipView mTipView;
+    private PopupWindow mPopupWindow;
 
-    private int[] mSeekBarLoc = new int[2];
-    private int[] mRootLoc = new int[2];
+    private Rect mSeekBarRect = new Rect();
+    private Rect mRootRect = new Rect();
     private int mSeekBarX;
-    private int mRootX;
+    private int mSeekBarY;
     private int mSeekBarPaddingStart;
     private int mSeekBarPaddingEnd;
+    private int TIP_VIEW_WIDTH = 60;
+    private int TIP_VIEW_HEIGHT = 72;
 
     public SelectTipSeekBar(Context context) {
         this(context, null);
@@ -46,18 +53,13 @@ public class SelectTipSeekBar extends FrameLayout implements View.OnTouchListene
     }
 
     public void initView(Context context){
-        mTipView = new SeekBarTipView(context);
-        FrameLayout.LayoutParams tipParams = new LayoutParams(DrawUtils.dp2px(60), DrawUtils.dp2px(72));
-        addView(mTipView, tipParams);
-        mTipView.setVisibility(GONE);
-
         mSeekBar = new SeekBar(context);
         mSeekBar.setProgressDrawable(getResources().getDrawable(R.drawable.bg_video_seekbar));
         mSeekBar.setThumb(getResources().getDrawable(R.drawable.seekbar_thumb_normal));
         FrameLayout.LayoutParams seekBarParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         addView(mSeekBar, seekBarParams);
-        seekBarParams.setMargins(0, DrawUtils.dp2px(100), 0, 0);
-
+        seekBarParams.setMargins(0, 0, 0, 0);
+        //反射设置SeekBar的高度
         try {
             Class<?> superclass = mSeekBar.getClass().getSuperclass().getSuperclass();
             Field mMaxHeight = superclass.getDeclaredField("mMaxHeight");
@@ -69,19 +71,19 @@ public class SelectTipSeekBar extends FrameLayout implements View.OnTouchListene
         mSeekBar.setMinimumHeight(DrawUtils.dp2px(2));
 
         mSeekBar.setOnTouchListener(this);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        initHintPopup();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        //获取进度条在屏幕中的起点横坐标
-        mSeekBar.getLocationOnScreen(mSeekBarLoc);
-        mSeekBarX = mSeekBarLoc[0];
-        //获取该自定义View在屏幕中的起点横坐标
-        getLocationOnScreen(mRootLoc);
-        mRootX = mRootLoc[0];
-        mSeekBarPaddingStart = mSeekBar.getPaddingStart();
-        mSeekBarPaddingEnd = mSeekBar.getPaddingEnd();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
     }
 
     /**
@@ -97,34 +99,79 @@ public class SelectTipSeekBar extends FrameLayout implements View.OnTouchListene
      * @param bitmap
      */
     public void setBitmap(Bitmap bitmap){
-        mTipView.setBitmap(bitmap);
+        if(mTipView != null){
+            mTipView.setBitmap(bitmap);
+        }
     }
+
+    /**
+     * 初始化提示弹框
+     */
+    private void initHintPopup() {
+        mTipView = new SeekBarTipView(getContext());
+        mPopupWindow = new PopupWindow(mTipView, DrawUtils.dp2px(TIP_VIEW_WIDTH), DrawUtils.dp2px(TIP_VIEW_HEIGHT), true);
+    }
+
+    /**
+     * 展示提示弹框
+     * @param x
+     * @param y
+     */
+    private void showPopup(float x, float y) {
+        if(mPopupWindow != null){
+            mPopupWindow.showAtLocation(mSeekBar, Gravity.NO_GRAVITY, (int)x, (int)y);
+        }
+
+    }
+
+    /**
+     * 随着滑动更新弹框位置与seekbar游标一致
+     * @param x
+     * @param y
+     */
+    private void updatePopup(float x, float y){
+        if(mPopupWindow != null){
+            mPopupWindow.update(mSeekBar, (int)x, (int)y, -1, -1);
+        }
+    }
+
+    /**
+     * 隐藏提示弹框
+     */
+    private void hidePopup() {
+        if (mPopupWindow != null && mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        }
+    }
+
 
 
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
+        //获取进度条在屏幕中的起点横坐标
+        mSeekBar.getGlobalVisibleRect(mSeekBarRect);
+        mSeekBarX = mSeekBarRect.left;
+        mSeekBarY = mSeekBarRect.top;
+        //获取该自定义View在屏幕中的起点横坐标
+        getGlobalVisibleRect(mRootRect);
+        mSeekBarPaddingStart = mSeekBar.getPaddingStart();
+        mSeekBarPaddingEnd = mSeekBar.getPaddingEnd();
         float x = motionEvent.getRawX();
-
-        float y = mSeekBar.getTop();
         if(x < mSeekBarX + mSeekBarPaddingStart || x > mSeekBarX + mSeekBar.getWidth() - mSeekBarPaddingEnd){
             return true;
         }
-        float curProgress = mSeekBar.getMax()*(x - mSeekBarX - mSeekBarPaddingStart)/(mSeekBar.getWidth() - mSeekBarPaddingStart - mSeekBarPaddingEnd);
+        float curProgress = mSeekBar.getMax()*(x - mSeekBarX - mSeekBarPaddingStart)*1.0f/(mSeekBar.getWidth() - mSeekBarPaddingStart - mSeekBarPaddingEnd)*1.0f;
         mSeekBar.setProgress((int)curProgress);
         switch (motionEvent.getAction()){
             case MotionEvent.ACTION_DOWN:
-                mTipView.setVisibility(View.VISIBLE);
-                //这里setX setY都是相对父布局左上顶点来作为原点计算
-                mTipView.setX(x - mTipView.getWidth()/2 - mRootX);
-                mTipView.setY(y -  mTipView.getHeight());
+                showPopup(x - mTipView.getWidth()/2, mSeekBarY -  mTipView.getHeight() - mSeekBar.getHeight());
                 break;
             case MotionEvent.ACTION_MOVE:
-                mTipView.setX(x - mTipView.getWidth()/2 - mRootX);
-                mTipView.setY(y - mTipView.getHeight());
+                updatePopup(x - mTipView.getWidth()/2 - mSeekBarX, - mTipView.getHeight() - mSeekBar.getHeight());
                 break;
             case MotionEvent.ACTION_UP:
-                mTipView.setVisibility(View.GONE);
+                hidePopup();
                 break;
         }
         return true;
